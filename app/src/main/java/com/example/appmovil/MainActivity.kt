41b76e86da17f342832ff0686b9a820424e77faa
Omartc13 <omartrc13@gmail.com>
2025-11-20 19:16:55 -0500

    Subiendo

---
+++ app/src/main/java/com/example/appmovil/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.appmovil
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var usernameInput : EditText
+    lateinit var passwordInput : EditText
+    lateinit var login_button : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        login_button = findViewById(R.id.login_button)
+
+        login_button.setOnClickListener{
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            Log.i("Test credentials", "Username: $username and Password: $password")
+
+            val intent : Intent = Intent(this, Galeria::class.java)
+            startActivity(intent)
+
+        }
+
+    }
+}
